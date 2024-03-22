656cbfd967792b6c9f18976584631decdc4ab2c7
Richard Konrad <projects.richardkonrad@gmail.com>
2024-03-22 15:35:58 +0100

    added gmmk neo-bone keymap

--- keyboards/gmmk/pro/rev1/iso/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/rules.mk
@@ -11,4 +11,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
